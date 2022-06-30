d4c1223662cf3b77041f098bb9526ea5521f51cc
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-06-30 16:07:29 -0700

    My new C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
+	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+	return (0);
+}
