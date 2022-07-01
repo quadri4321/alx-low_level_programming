390b340822878e5fc37ab848c8beaff050c6d772
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-06-30 17:05:06 -0700

    New C programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main-Entry point
  * print word to output
  * Return: Always 0 (success)
- */
-
-int main (void)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
  * main-Entry
  *
  * Return: Always 0 (success)
- */
+*/
 int main(void)
 {
 	int a;
@@ -13,10 +12,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(a));
-	printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
