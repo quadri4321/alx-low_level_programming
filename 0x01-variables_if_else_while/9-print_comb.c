5b31073830ac6591470c663c9874ff54948305c4
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-07-03 03:41:49 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int i;
-
 for (i = 48; i <= 57; i++)
 {
 putchar(i);
 if (i != 57)
+{
 putchar(44);
 putchar(32);
 }
@@ -19,5 +19,7 @@ else
 {
 putchar(10);
 }
+}
 return (0);
 }
+
