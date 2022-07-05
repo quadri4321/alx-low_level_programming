7724b9234c7446289aded531d8a84bda9f505233
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-07-05 15:46:48 -0700

    new c programs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ * C program to prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0.
+**/
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar("\n");
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main- Entry point
+ * C programs that prints 10 times the alphabet, in lowercase
+ * Return: 0 (if successful)
+**/
+int main(void)
+{
+int j;
+int k;
+
+for (k = 0; k <= 10; k++)
+{
+for (j = 'a'; j <= 'z' j++)
+{
+_putchar(j);
+}
+_putchar("\n")
+}
+}
+
+
+
+
