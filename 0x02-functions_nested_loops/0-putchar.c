7fe978d4aaebc1711b8969512a550e61f44c3c1b
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-07-05 23:32:30 -0700

    edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,7 @@
 **/
 int main(void)
 {
+	
 	putchar('_');
 	putchar('p');
 	putchar('u');
@@ -13,7 +14,7 @@ int main(void)
 	putchar('h');
 	putchar('a');
 	putchar('r');
-	putchar("\n");
+	putchar('\n');
 
 	return(0);
 }
