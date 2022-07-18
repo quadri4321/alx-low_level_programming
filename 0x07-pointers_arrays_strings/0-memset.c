60f2d72d79d98e90b643acbe25c14311a6ad1906
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-07-18 13:53:39 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+unsigned int size = 0; 
 
-for (i = 0; i < n; i++)
+while (size < n)
 {
-s[i] = b;
+*(s + size) = b;
+size++;
 }
-
 return (s);
 }
