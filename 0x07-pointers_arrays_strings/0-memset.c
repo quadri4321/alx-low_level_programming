1423de4997efa034af291ac94edd2044b6fa9c51
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-07-18 14:11:29 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,11 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
- *
  * Return: *s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int size = 0; 
+unsigned int size = 0;
 
 while (size < n)
 {
