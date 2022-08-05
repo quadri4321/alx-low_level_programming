ec78b90a5ebaa3cc48314c34a71010a543a4c9b3
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-08-05 16:53:55 -0700

    task 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,11 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: amount of the arguments.
- *
+ * @...: Other parameters
  * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
