fc76c5ac0e025fecd56d281f538195e5f182bd6c
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-08-16 20:09:10 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,39 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node at an index
- * @head: pointer to the head of the list
- * @index: index of the node to be added
- * Return: the address of the node
+ * delete_nodeint_at_index - Deletes the node at a given
+ * index of a listint_t list.
+ * @head: A pointer to the address of the
+ * head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
+ * Return: On success - 1.
+ * On failure - -1.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *old_node = NULL;
-listint_t *previous_node = NULL;
-unsigned int i = 0, list_len = listint_len(*head);
+listint_t *tmp, *copy = *head;
+unsigned int node;
 
-if ((index > list_len) || (list_len == 0))
+if (copy == NULL)
 return (-1);
-while (head != NULL)
+
+if (index == 0)
 {
-if (i == index)
-{
-old_node = *head;
-if (i == 0)
-{
-*head = old_node->next;
-ree(old_node);
+*head = (*head)->next;
+free(copy);
 return (1);
 }
-previous_node->next = old_node->next;
-free(old_node);
-return (1);
-}
-else if ((i + 1) == index)
-previous_node = *head;
-head = &((*head)->next);
-i++;
-}
+
+for (node = 0; node < (index - 1); node++)
+{
+if (copy->next == NULL)
 return (-1);
+
+copy = copy->next;
+}
+
+tmp = copy->next;
+copy->next = tmp->next;
+free(tmp);
+return (1);
 }
