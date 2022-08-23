d769da299e63d2651a9a421ef2b55260780adf16
Adejumo Quadri Akanbi <quadrifirst2@gmail.com>
2022-08-22 21:36:33 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,39 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- * Return: If the function fails or filename is NULL -1
- * if the file does not exist the user lacks write permissions -1
- * otherwise 1
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ * Return: length of the string
  */
-
-int append_text_to_file(const char *filename, char *text_content)
+size_t _strlen(char *str)
 {
-int o, w, len = 0;
+size_t i;
+
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t len = 0;
 
 if (filename == NULL)
 return (-1);
-
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
-}
-
-o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
-
-if (o == -1 || w == -1)
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
 return (-1);
-
-close(o);
-
+if (text_content != NULL)
+len = write(fd, text_content, _strlen(text_content));
+close(fd);
+if (len == -1)
+return (-1);
 return (1);
 }
